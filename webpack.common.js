707b91c34294f94535a491e4731fdb58e371d0fb
Pyroblazer <darkshadow.itm777@gmail.com>
2023-12-02 19:35:44 +0700

    updated build and deploy config

--- webpack.common.js
+++ webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     clean: true,
   },
   module: {
@@ -82,7 +82,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          to: path.resolve(__dirname, 'public/'),
         },
       ],
     }),
